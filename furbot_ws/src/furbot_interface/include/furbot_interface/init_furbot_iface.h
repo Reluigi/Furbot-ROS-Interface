eb5724cd5935dd947c4454800cb848082cecf7ed
Ruslan Aminev <amirusrif@gmail.com>
2018-07-16 12:59:49 +0200

    init exec scheme and include

---
+++ furbot_ws/src/furbot_interface/include/furbot_interface/init_furbot_iface.h
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
---
+++ furbot_ws/src/furbot_interface/src/init_furbot_iface.cpp
@@ -0,0 +1,17 @@
+#include "init_furbot_iface.h"
+
+int main (){
+    // Load configuration
+
+    // Try to connect
+
+    // Check statuses of in status frame
+
+    // Fork status publicher
+
+    // Fork control subscriber
+
+    // Wait for processes to finish, free memory
+
+    return 0;
+}
