86af70fd04a59d4225ab9924c54e98e77c2fa304
ReLuigi <luigi@isecondo.eu>
2018-07-23 18:53:53 +0200

    Improved odometry and solved many errors

--- furbot_ws/src/furbot_interface/src/furbot_odometry.cpp
+++ furbot_ws/src/furbot_interface/src/furbot_odometry.cpp
@@ -2,68 +2,79 @@
 #include <tf/transform_broadcaster.h>
 #include <nav_msgs/Odometry.h>
 #include "std_msgs/String.h"
+#include "furbot_msgs/TractionData.h"
+#include <iostream>
+
+furbot_msgs::TractionData msg;
 
 
-void chatterCallback(const std_msgs::String::ConstPtr& msg)
+void odometryCallback(const furbot_msgs::TractionData::ConstPtr& msgl)
 {
-  ROS_INFO("I heard: [%s]", msg->data.c_str());
+	msg.vel_r = msgl->vel_r;
+	msg.vel_l = msgl->vel_l;
+	//ROS_INFO("Rear wheels angular speed is: [%s]RL [%s]RR", msg.vel_l, msg.vel_r);
+	std::cout << "Angular RL: " << msg.vel_l << ", RR: " << msg.vel_r << "\n";
+
 }
 
 int main(int argc, char** argv){
-  ros::init(argc, argv, "odometry_publisher");
+	msg.vel_l = 0;
+	msg.vel_r = 0;
 
-  ros::NodeHandle n;
-  ros::Publisher odom_pub = n.advertise<nav_msgs::Odometry>("odom", 50);
-  tf::TransformBroadcaster odom_broadcaster;
+	ros::init(argc, argv, "odometry_publisher");
+
+	ros::NodeHandle n;
+	ros::Publisher odom_pub = n.advertise<nav_msgs::Odometry>("furbot/odom", 50);
+	tf::TransformBroadcaster odom_broadcaster;
+
+	//getting traction data subscribing to TractionData message
+	ros::Subscriber sub = n.subscribe("furbot/traction_data", 1000, odometryCallback);
+
+	//furbot specification
+	double radius = 0.3; //wheel radius
+	double L = 0.6425; //distance between wheel and origin of robot frame
+	double pi = 3.1415926535;
+
+
+	//v = 2πr × RPM × (60/1000) km/hr
+
+	double x = 0.0;
+	double y = 0.0;
+	double th = 0.0;
+
+	double vx = 0.1;
+	double vy = -0.1;
+	double vth = 0.1;
+
   
-  //getting traction data subscribing to TractionData message
-  ros::Subscriber sub = n.subscribe("TractionData", 1000, chatterCallback);
+	//assuming furbot works in rad for angles
 
-  //furbot specification
-  double radius = 0.3; //wheel radius
-  double L = 0.6425; //distance between wheel and origin of robot frame
-  double pi = 3.1415926535;
+	ros::Time current_time, last_time;
+	current_time = ros::Time::now();
+	last_time = ros::Time::now();
 
-  
-  //v = 2πr × RPM × (60/1000) km/hr
+	ros::Rate r(1.0);
+	while(n.ok()){
 
-  double x = 0.0;
-  double y = 0.0;
-  double th = 0.0;
+	ros::spinOnce();               // check for incoming messages
+	current_time = ros::Time::now();
 
-  double vx = 0.1;
-  double vy = -0.1;
-  double vth = 0.1;
+	/*
+	//compute odometry in a typical way given the velocities of the robot
+	double dt = (current_time - last_time).toSec();
+	double delta_x = (vx * cos(th) - vy * sin(th)) * dt;
+	double delta_y = (vx * sin(th) + vy * cos(th)) * dt;
+	double delta_th = vth * dt;
+	*/
 
-  
-//assuming furbot works in rad for angles
+	//converting rear speeds in linear velocities (from RPM to m/s)
+	// m/s = 60*RPM/(4*pi^2*r)
+	double vel_r_lin = (60*msg.vel_r)/(4*pi*pi*radius);
+	double vel_l_lin = (60*msg.vel_l)/(4*pi*pi*radius);
 
-  ros::Time current_time, last_time;
-  current_time = ros::Time::now();
-  last_time = ros::Time::now();
-
-  ros::Rate r(1.0);
-  while(n.ok()){
-
-    ros::spinOnce();               // check for incoming messages
-    current_time = ros::Time::now();
-
-    /*
-    //compute odometry in a typical way given the velocities of the robot
-    double dt = (current_time - last_time).toSec();
-    double delta_x = (vx * cos(th) - vy * sin(th)) * dt;
-    double delta_y = (vx * sin(th) + vy * cos(th)) * dt;
-    double delta_th = vth * dt;
-    */
-
-    //converting rear speeds in linear velocities (from RPM to m/s)
-    // m/s = 60*RPM/(4*pi^2*r)
-    double vel_r_lin = (60*sub.vel_r)/(4*pi*pi*radius);
-    double vel_l_lin = (60*sub.vel_l)/(4*pi*pi*radius);
-
-    //compute v and w using matlab formulas (challenge emaro days)
-    double v = (vel_r_lin + vel_l_lin)/2;
-    double w = (vel_l_lin - vel_r_lin)/L;
+	//compute v and w using matlab formulas (challenge emaro days)
+	double v = (vel_r_lin + vel_l_lin)/2;
+	double w = (vel_l_lin - vel_r_lin)/L;
 
     /*
     //furbot [v, w] computation
@@ -90,35 +101,35 @@ int main(int argc, char** argv){
     odom_trans.header.frame_id = "odom";
     odom_trans.child_frame_id = "base_link";
 
-    odom_trans.transform.translation.x = x;
-    odom_trans.transform.translation.y = y;
-    odom_trans.transform.translation.z = 0.0;
-    odom_trans.transform.rotation = odom_quat;
+	odom_trans.transform.translation.x = x;
+	odom_trans.transform.translation.y = y;
+	odom_trans.transform.translation.z = 0.0;
+	odom_trans.transform.rotation = odom_quat;
 
-    //send the transform
-    odom_broadcaster.sendTransform(odom_trans);
+	//send the transform
+	odom_broadcaster.sendTransform(odom_trans);
 
-    //next, we'll publish the odometry message over ROS
-    nav_msgs::Odometry odom;
-    odom.header.stamp = current_time;
-    odom.header.frame_id = "odom";
+	//next, we'll publish the odometry message over ROS
+	nav_msgs::Odometry odom;
+	odom.header.stamp = current_time;
+	odom.header.frame_id = "odom";
 
-    //set the position
-    odom.pose.pose.position.x = x;
-    odom.pose.pose.position.y = y;
-    odom.pose.pose.position.z = 0.0;
-    odom.pose.pose.orientation = odom_quat;
+	//set the position
+	odom.pose.pose.position.x = x;
+	odom.pose.pose.position.y = y;
+	odom.pose.pose.position.z = 0.0;
+	odom.pose.pose.orientation = odom_quat;
 
-    //set the velocity
-    odom.child_frame_id = "base_link";
-    odom.twist.twist.linear.x = vx;
-    odom.twist.twist.linear.y = vy;
-    odom.twist.twist.angular.z = vth;
+	//set the velocity
+	odom.child_frame_id = "base_link";
+	odom.twist.twist.linear.x = vx;
+	odom.twist.twist.linear.y = vy;
+	odom.twist.twist.angular.z = vth;
 
-    //publish the message
-    odom_pub.publish(odom);
+	//publish the message
+	odom_pub.publish(odom);
 
-    last_time = current_time;
-    r.sleep();
-  }
+	last_time = current_time;
+	r.sleep();
+	}
 }
